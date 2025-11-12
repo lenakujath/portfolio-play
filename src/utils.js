8b30537f51bd18ba9e97b7dbbfbf05cca37e5bf1
lenakujath <l.kujath@gmx.de>
2025-11-12 18:48:09 +0100

    switch deprecated method

--- src/utils.js
+++ src/utils.js
@@ -30,13 +30,11 @@ export function displayDialogue(text, onDisplayEnd) {
     closeBtn.addEventListener("click", onCloseBtnClick);
 }
 
-
 export function setCamScale(k) {
     const resizeFactor = k.width() /k.height();
     if (resizeFactor < 1) {
-        k.camScale(k.vec2(1));
+        k.getCamScale(k.vec2(1));
         return;
     }
-
-    k.camScale(k.vec2(1.5))
+    k.getCamScale(k.vec2(1.5))
 }
