4aabaf68f70003c1e2bb2cf19807a993754a35f9
lenakujath <l.kujath@gmx.de>
2025-12-17 13:20:51 +0100

    work on texts

--- src/constants.js
+++ src/constants.js
@@ -3,35 +3,38 @@ export const scaleFactor = 4;
 export const dialogueData = {
   pc: `As a programmer, naturally this is where most of my work takes place. Since I work in web development, I am mostly familiar with 
         JavaScript and some of its popular libraries. I also worked with PHP and some Content Management Systems like WordPress, which 
-        rely on it. I also think it's important to not forget about the very fundamentals in web dev, so regularyly brushing up on HTML and CSS knwoledge
-        and keeping up to date is crucial as well. 
+        rely on it. I also think it's important to not forget about the very fundamentals of web dev, 
+        so regularyly brushing up on HTML and CSS knwoledge
+        and keeping up to date is crucial. 
         Besides it being my day job, coding is also just a fun and rewarding activity for me, that's one of the reasons I created this site. 
-        You can checkout some of my "leisure" code on <a href="https://github.com/lenakujath" target="_blank">Github</a>, feel free to browse 
+        You can checkout some of my "leisure" code on <a href="https://github.com/lenakujath" target="_blank">Github</a>, 
+        feel free to browse 
         through!`,
   desk: `Even if I did not ending up working in a field directly related to my studies (arts, music and media), I am glad I chose 
-                    and finished it successfully. Besides learning some valuable meta skills like organizing my time and gaining an understanding of a previously 
-                    unknown field, I really enjoyed being surrounded by like minded people and to deepen my knowledge in the respective topics.
-                    Later, when I took on developing software, I even noticed some similarities in the way my brain works when putting together some 
-                    code and analzing or arranging music.`,
+            and finished it successfully. Besides learning some valuable meta skills like organizing my time and gaining 
+            an understanding of a previously 
+            unknown field, I really enjoyed being surrounded by like minded people and to deepen my knowledge in the respective 
+            topics.
+            Later, when I took on developing software, I even noticed some similarities in the way my brain works 
+            when putting together some 
+            code and analzing or arranging music.`,
   "sofa": `After a full day I like to relax on the sofa and maybe listen to music, play my guitar or watch a good movie.`,
   tv: `I rarely browse through regular tv but I do enjoy a good movie or a great series. I am also a big fan of learning
         about new things by watching some cool channels on youtube. To create games with JavaScript 
         <a href="https://youtube.com/@jslegenddev" target="_blank">JSLegendDev</a> is an excellent resource. It's how I learned 
         how to use Kaplay to create this website :).`,
   bed: `Rest is important, so I try to get enough of it every night. Sometimes the solution to a problem presents itself in the
-        moments I'm most relaxed. I think lots of people can relate.
-         I often keep some notebooks next to my bed, so these ideas don't get lost again.`,
+        moments I'm most relaxed. You can probably relate :)
+         I often keep some notebooks next to my bed, so these ideas don't get lost, as they unfortunately do so often.`,
   bag: `It's important to always be prepared, so I keep my bag ready and packed!`,
   fridge: `I love as much to eat as the next person so it's important to me to have my fridge filled with ingredients to prepare
             a delious meal. I also enjoy eating it a lot to eat out with friends and family.`,
   guestbed: `I love being surrounded by my friends and to have them over. There is always a place for them to stay in my flat.`,  
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
   garden: `Programming and computers are nice, but it's also important to get some fresh air. I am a big fan of just strolling around 
             outside, be it in the city or in nature.`,
-  bookshelf: `I love to read since I'm a kid. I like to read books to learn things of course, but I also just like to read a 
-              good novel.`,
-  exit: `"You can checkout any time you like...BUT YOU CAN NEVER LEAVE - Just kidding, if you want to exit this site, just close 
+  bookshelf: `I always loved to read, from childhood on. I like to read books to learn things of course, but doing it just for the
+                  enjoyment of it is just as valid for me.`,
+  exit: `"You can checkout any time you like...BUT YOU CAN NEVER LEAVE" - Just kidding, if you want to exit this site, just close 
            your current tab.`,
 };
 
